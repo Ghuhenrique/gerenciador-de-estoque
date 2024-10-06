476b7b610ae1796c03aaa80e3e98280733ebe3ec
Gustavo Henrique <gustavohcp@outlook.com>
2024-10-06 20:21:07 -0300

    Commit inicial

---
+++ src/gerenciadorDeArquivos/Main.java
@@ -0,0 +1,17 @@
+package gerenciadorDeArquivos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*Desenvolver um sistema para gerenciamento de estoque que permita o controle de entrada e saíde de  produtos. O sistema deverá permitir ao usuário cadastrar novos produtos, atualizar quantidades em estoque, registrar saídas (vendas ou usos) e gerar relatórios de estoque com base nos dados armazenados. 
+		Deve haver um arquivo de texto ou CSV que armazene os dados dos produtos, incluído nome, código, quantidade, preço e data de validade (opcional);
+		O sistema deve permitir a busca de produtos por nome ou código;
+		Um relatório de inventário deve ser gerado mostrando o status atual do estoque (produtos disponíveis, em falta e etc.).
+		O programa deverá ser desenvolvido sem o uso de conceitos de programação orientada a objetos e cada implementação deve envolver o uso de: estruturas de controle (condicionais, laços); funções (modularização); manipulação de arquivos (leitura e escrita); estrutura de dados simples (arrays, vetores, matrizes).
+		*/
+		//Alunos: Gustavo Henrique Costa Pinto
+		
+
+	}
+
+}
